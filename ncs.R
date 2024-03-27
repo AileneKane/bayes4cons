912a3cd066f4071b980ace772c01c5bd9b1ec9fb
AileneKane <ailene.ettinger@gmail.com>
2024-03-27 13:11:52 -0700

    new figs and adding co2

--- ncs.R
+++ ncs.R
@@ -29,8 +29,9 @@ library(brms)
 # Approach: We simulate flux data from a field study
 # quantifying GHG fluxes in peatlands with and without grazing
 # in Ecuador (Sanchez et al) 
-# Simulate CH4 fluxes in grazed vs ungrazed peatlands 
+# Simulate CH4 & CO2 fluxes in grazed vs ungrazed peatlands 
 # set mean and variation in grazed and ungrazed locations
+# forecast effects of drought in grazed and ungrazed locations
 #############################################################
 
 ch4mn_graz<- 132.25# mg CH4 m^-2 d^-1 grazed
@@ -68,10 +69,51 @@ plot(ch4est)
 # Ungrazed area (b_luungr) is 4.08 
 
 # save the full posterior distribution/samples for later use (i.e. to propagate the estimated uncertainty)
-graz_samples <-posterior_samples(ch4est)$b_Intercept
-ungr_samples <-posterior_samples(ch4est)$b_Intercept+posterior_samples(ch4est)$b_luungr
+ch4graz_samples <-posterior_samples(ch4est)$b_Intercept
+ch4ungr_samples <-posterior_samples(ch4est)$b_Intercept+posterior_samples(ch4est)$b_luungr
 
 # perhaps newer syntax is as_draws(ch4est)?
+
+# co2
+co2mn_graz<- -1.25# g CO2 m^-2 hr^-1 grazed
+co2sd_graz<- 0.13  
+co2mn_ungr<-1.19#  g CO2 m^-2 hr^-1 ungrazed
+co2sd_ungr<- 0.12
+
+# simulate field data using the means, sds
+co2dat <- as.data.frame(cbind(
+  c(rep("graz",times=nreps),rep("ungr",times=nreps)),
+  c(rnorm(n = nreps, mean = co2mn_graz, sd = co2sd_graz),
+    rnorm(n = nreps, mean = co2mn_ungr, sd = co2sd_ungr))
+))
+colnames(co2dat)<-c("lu","co2")
+co2dat$co2<-as.numeric(co2dat$co2)
+co2dat$lu<-as.factor(co2dat$lu)
+
+# convert g CO2 m^-2 hr^-1 to g co2 ha^-yr
+co2dat$co2_hayr<-as.numeric(co2dat$co2)*0.8760
+
+# Note that the simulated data could be mademore complex 
+# e.g., through adding variation in space and time within different landuses
+
+# Fit a simple Bayesian linear model with landuse as a predictor of CH4 flux
+# use the brms package (https://paul-buerkner.github.io/brms/)
+
+co2est<-brm(co2_hayr~lu, data=co2dat)
+
+# check the model
+summary(co2est)
+plot(co2est)
+
+# Estimate ch4 flux in grazed area (b_Intercept) is 4.43 (95% uncertainty intervals: 4.09-4.73) 
+# Ungrazed area (b_luungr) is 4.08 
+
+# save the full posterior distribution/samples for later use (i.e. to propagate the estimated uncertainty)
+co2graz_samples <-posterior_samples(co2est)$b_Intercept
+co2ungr_samples <-posterior_samples(co2est)$b_Intercept+posterior_samples(co2est)$b_luungr
+
+# perhaps newer syntax is as_draws(ch4est)?
+
 
 #############################################################
 # EXTENT (LANDUSE AREA)
@@ -84,8 +126,8 @@ ungr_samples <-posterior_samples(ch4est)$b_Intercept+posterior_samples(ch4est)$b
 # Hypothetical project area extent
 ext<-30000#ha
 
-ch4emiss_ungr <-ungr_samples*ext*0.001#*0.001 is to convert to g from mg 
-ch4emiss_graz <-graz_samples*ext*0.001
+ch4emiss_ungr <-ch4ungr_samples*ext*0.001#*0.001 is to convert to kg from g 
+ch4emiss_graz <-ch4graz_samples*ext*0.001
 
 ch4means<-c(mean(ch4emiss_graz),mean(ch4emiss_ungr))
 ch4meds<-c(median(ch4emiss_graz),median(ch4emiss_ungr))
@@ -93,12 +135,12 @@ uncert<-cbind(quantile(ch4emiss_graz,c(.10,.90)),
               quantile(ch4emiss_ungr,c(.10,.90)))
 colnames(uncert)<-c("graz","ungr")
 x<-c(1,2)
-png("ncsprojimpact.png",height=600,width=800)
+png("ncsprojimpactch4.png",height=600,width=800)
 plot(x,ch4meds,
      type="p", pch=16, cex=3,col="darkgreen",
      ylim=c(0,150),xlim=c(.5,2.5),xaxt='n',cex.axis=2,
-     ylab="CH4 (g per ha per yr)",xlab="Land Use",cex.lab=2,
-     main="GHG Emissions in Project Area",
+     ylab="CH4 (kg per ha per yr)",xlab="Land Use",cex.lab=2,
+     main="CH4 Emissions in Project Area",
      bty="l")
 for(i in 1:length(x)){
   arrows(x[i],uncert[1,i],x[i],uncert[2,i], 
@@ -106,6 +148,41 @@ for(i in 1:length(x)){
 }
 axis(1,at=x,labels=c("grazed","ungrazed"), cex.axis=2)
 dev.off()
+
+#Now CO2
+
+co2emiss_ungr <-co2ungr_samples*ext*.001#convert to kg 
+co2emiss_graz <-co2graz_samples*ext*.001
+
+co2means<-c(mean(co2emiss_graz),mean(co2emiss_ungr))
+co2meds<-c(median(co2emiss_graz),median(co2emiss_ungr))
+co2uncert<-cbind(quantile(co2emiss_graz,c(.10,.90)),
+              quantile(co2emiss_ungr,c(.10,.90)))
+colnames(uncert)<-c("graz","ungr")
+
+png("ncsprojimpactco2.png",height=600,width=800)
+plot(x,co2meds,
+     type="p", pch=16, cex=3,col="darkgreen",
+     ylim=c(-50,50),xlim=c(.5,2.5),xaxt='n',cex.axis=2,
+     ylab="CO2 (T per ha per yr)",xlab="Land Use",cex.lab=2,
+     main="CO2 Emissions in Project Area",
+     bty="l")
+for(i in 1:length(x)){
+  arrows(x[i],co2uncert[1,i],x[i],co2uncert[2,i], 
+         code=3, angle=90, length=0.05,lwd=2, col="darkgreen")
+}
+axis(1,at=x,labels=c("grazed","ungrazed"), cex.axis=2)
+dev.off()
+
+# Now forecast future fluxes, under warming/drought (i.e. lower water table)
+# From Wilson et al, CO2 flux vs MWTL
+# co2 = -0.29 + (-0.05× MWTL))
+# current water table is (-3, -5) for ungrazed and (0,20) for grazed 
+
+
+	
+#projected increase in CO2 emissions from peatlands with decreased water table in grazed and ungrazed areas
+
 
 # for now fit to all of peat area in ecuador. 
 # could consider implementing for some arbitraty area- e.g., restricting grazing
