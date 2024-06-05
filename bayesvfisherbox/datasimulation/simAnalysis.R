04afd43238085bff52b86a4e9659278f9ba2d6ae
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2024-06-05 15:48:39 -0700

    added legend and means nht fig

--- bayesvfisherbox/datasimulation/simAnalysis.R
+++ bayesvfisherbox/datasimulation/simAnalysis.R
@@ -18,6 +18,7 @@ if(length(grep("deirdreloughnan", getwd())>0)) {
 require(rstan)
 require(lme4)
 require(shinystan)
+require(viridis)
 
 # Taking the simulation code Mao and Lizzie wrote:
 set.seed(1546)
@@ -44,6 +45,9 @@ abund <- data.frame(reshape2::melt(t(abund)))
 output$pred <- abund$value
 
 par(mfrow = c(3,3))
+popID <- unique(output$pop)
+colors <- c("#f7cb44ff", "#f68f46ff", "#de7065ff", "#a65c85ff","#593d9cff","purple4", "navy")
+
 for(i in 1:length(unique(output$pop))){
   temp <- subset(output, pop == popID[i])
   plot(pred ~ year, data = temp, 
@@ -57,7 +61,7 @@ for(i in 1:length(unique(output$pop))){
   abline(lm(pred ~ year, temp), lty = 2, col = colors[i], lwd = 1.5)
 }
 
-# To better fit the narrative, excluding 2nd and 5th pop:
+# To better fit the narrative, excluding 2nd and 6th pop:
 output <- subset(output, pop != 2 & pop != 6)
 str(output)
 output$pop <- as.factor(output$pop)
@@ -65,26 +69,78 @@ output$pop <- as.factor(output$pop)
 datalistGrp <- with(output,
   list( N = nrow(output),
     Ngrp = length(unique(output$pop)),
-    group = as.numeric(output$pop),
+    group = as.numeric(as.factor(output$pop)),
     ypred = output$pred,
     year = output$year ))
 
 
-mdlPop<- stan("bayesvfisherbox/stan/partialPoolSimMdl.stan",
+mdlPop <- stan("bayesvfisherbox/stan/partialPoolSimMdl.stan",
   data = datalistGrp)
 
-save(mdlPop, file="bayesvfisherbox/output/simIntPop1546.Rdata")
+#save(mdlPop, file="bayesvfisherbox/output/simIntPop1546.Rdata")
 
-sum <- (summary(mdlPop)$summary)
+sum <- summary(mdlPop)$summary
 
 intercept <- sum[grep("a\\[", rownames(sum)), "mean"]
 slopes <- sum[grep("b\\[", rownames(sum)), "mean"]
 
 post <- rstan::extract(mdlPop)
-popID <- unique(output$pop)
-colors <- c("#593d9cff","#a65c85ff", "#de7065ff", "#f68f46ff", "#f7cb44ff")
+
+pdf("bayesvfisherbox/figures/nhtBoxLayeredMeans.pdf", height = 6, width = 12)
+colfunc <- colorRampPalette(c("#593d9cff","#a65c85ff", "#de7065ff", "#f68f46ff","#f7cb44ff"))
+legend_image <- as.raster(matrix(colfunc(5), ncol=1))
+
+layout(matrix(1:2,ncol=2), width = c(2,1),height = c(1,1))
+par(mfrow = c(1,2), mar = c(5.1, 4.5, 4.1, 2.1))
+plot(1, type="n", ylab = "Abundance", xlab = "Year", xlim=c(00, 10), ylim=c(0, 160000), 
+  cex.lab = 1.5, frame.plot = FALSE, xaxt = "n", yaxt = "n")
+axis(side = 1, at = seq(-3000,3000, by = 1), cex.axis =1)
+axis(side = 2, at = seq(-1000000,1000000, by = 20000), cex.axis =1)
+for(i in 1:length(unique(output$pop))){
+  temp <- subset(output, pop == popID[i])
+  points(temp$year, temp$pred, col = colors[i], pch = 19)
+}
+abline(lm(pred ~ year, subset(output, pop == popID[5])), lty = 4, col = colors[5], lwd = 1.5)
+abline(lm(pred ~ year, subset(output, pop == popID[4])), lty = 4, col = colors[4], lwd = 1.5)
+
+legend("topleft",legend = c(expression('Farthest north (p = 3.4e'^-4*')'),
+  expression('North (p = 0.01)'),
+  expression('Middle (p = 0.36)'),
+  expression('South (p = 0.15)'),
+  expression('Farthest south (p = 0.08)')),
+  lty = c(1, 1, 1, 1, 1), lwd = 4, bty = "n", col =  c("#593d9cff","#a65c85ff", "#de7065ff", "#f68f46ff","#f7cb44ff"))
 
 
+plot(1, type="n", xlab = "Change in abundance", ylab = "Frequency", xlim=c(-2500, 2500), ylim=c(0, 1500), cex.lab = 1.5, frame.plot = FALSE)
+for(i in 1:length(unique(output$pop))){
+
+  hist(post$b[,i], add = T, col = colors[i] )
+  axis(side = 1, at = seq(-3000,3000, by = 1000), cex.axis =1)
+  axis(side = 2, at = seq(-1000000,1000000, by = 1000), cex.axis =1)
+  points(x = mean(post$b[,i]), y = 1300, col = colors[i], pch = 19)
+  arrows(x0 = (quantile(post$b[,i], 0.05)), y0= (1300), x1= (quantile(post$b[,i], 0.95)), y1= 1300, code = 3, length = 0, col = colors[i], lwd=2)
+}
+
+
+# text(2300, 2050, "Northern")    
+# text(2300, 1450, "Southern")    
+# 
+# op <- par(  ## set and store par
+#   fig=c(grconvertX(c(2000,2050), from="user", to="ndc"),   
+#     grconvertY(c(1500,2000), from="user", to="ndc")), 
+#   mar=c(0.05, 2, 0.01, 0.5),                                 
+#   new=TRUE)                               
+# plot.window(c(0, 2), c(0, 1))                           
+# rasterImage(legend_image, 0, 0, 1, 1)                      
+# par(op)  
+# 
+# 
+dev.off()
+
+
+################################################
+# scrape code and older figures
+# 5 x 2 panel figure
 pdf("bayesvfisherbox/figures/nhtBox.pdf", height = 12, width = 6)
 par( mar = c(5.1, 4.5, 4.1, 2.1))
 layout(matrix(c(1, 2, 3, 4, 5, 1,2,3,4,5,6,7,8,9,10), ncol=3))
@@ -182,40 +238,7 @@ for(i in 1:length(unique(output$pop))){
   axis(side = 2, at = seq(-1000000,1000000, by = 1000), cex.axis =1)
 }
 dev.off()
-################################################
 
-
-pdf("bayesvfisherbox/figures/nhtBoxLayered.pdf", height = 5, width = 12)
-par(mfrow = c(1,2), mar = c(5.1, 4.5, 4.1, 2.1))
-plot(1, type="n", ylab = "Abundance", xlab = "Year", xlim=c(00, 10), ylim=c(0, 150000), cex.lab = 1.5)
-for(i in 1:length(unique(output$pop))){
-  temp <- subset(output, pop == popID[i])
-  points(temp$year, temp$pred, col = colors[i], pch = 19)
-}
-abline(lm(pred ~ year, subset(output, pop == popID[5])), lty = 4, col = colors[5], lwd = 1.5)
-abline(lm(pred ~ year, subset(output, pop == popID[4])), lty = 4, col = colors[4], lwd = 1.5)
-
-plot(1, type="n", xlab = "Change in abundance", ylab = "Frequency", xlim=c(-2500, 2500), ylim=c(0, 2000), cex.lab = 1.5)
-for(i in 1:length(unique(output$pop))){
-hist(post$b[,i], add = T, col = colors[i] )
-axis(side = 1, at = seq(-3000,3000, by = 1000), cex.axis =1)
-axis(side = 2, at = seq(-1000000,1000000, by = 1000), cex.axis =1)
-}
-
-# gradientLegend(
-#   x = 0.5, y = 0.5,
-#   legend = output$pop,
-#   fill = colorRampPalette(colors)(11),
-#   border = NA,
-#   y.intersp = 0.5,
-#   cex = 2, text.font = 2)
-# # legend("topright",legend = c("Pop 1", "Pop 2", "Pop 3", "Pop 4", "Pop 5"),
-# #   lty = c(1, 1, 1, 1, 1), lwd = 4, bty = "n", col =  c("#CC6677", "cyan4", "purple4", "goldenrod", "darkgreen","sienna", "navy"))
-
-dev.off()
-
-################################################
-# scrape
 pdf("bayesvfisherbox/figures/tempFighHistPoints.pdf", width = 6, height = 6)
 jit <- c(0, 30,60,90,120)
 par(mfrow = c(1,1), mar = c(5.1, 4.5, 4.1, 2.1))
@@ -229,8 +252,10 @@ for(i in 1:length(unique(output$pop))){
 }
 
 legend("topright",legend = c("Pop 1", "Pop 2", "Pop 3", "Pop 4", "Pop 5"),
-  lty = c(1, 1, 1, 1, 1), lwd = 4, bty = "n", col =  c("#CC6677", "cyan4", "purple4", "goldenrod", "darkgreen","sienna", "navy"))
+  lty = c(1, 1, 1, 1, 1), lwd = 4, bty = "n", col =  c("#593d9cff","#a65c85ff", "#de7065ff", "#f68f46ff","#f7cb44ff"))
 dev.off()
+
+
 temp <- subset(output, pop == popID[1])
 plot(pred ~ year, data = temp, 
   frame.plot = F,
